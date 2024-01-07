97ff23459cb78fda3ea8a2f108256d5ec6fb30b6
andxbes <andxbes@gmail.com>
2024-01-07 23:41:18 +0200

    fix class

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -53,9 +53,9 @@ function Blog() {
                         <Notice title={'Error'} text="Something went wrong"></Notice>
                     )}
 
-                    <div class="flex flex-1 justify-between sm:justify-end">
-                        <button onClick={() => setPage((p) => p > 1 ? p - 1 : 1)} class="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0">Previous</button>
-                        <button onClick={() => setPage((p) => p + 1)} class="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0">Next</button>
+                    <div className="flex justify-between flex-1 sm:justify-end">
+                        <button onClick={() => setPage((p) => p > 1 ? p - 1 : 1)} className="relative inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 bg-white rounded-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0">Previous</button>
+                        <button onClick={() => setPage((p) => p + 1)} className="relative inline-flex items-center px-3 py-2 ml-3 text-sm font-semibold text-gray-900 bg-white rounded-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0">Next</button>
                     </div>
                 </div>
             </div>
